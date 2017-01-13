5df672afc029e498452f322b79a73173a5158b52
dotheanhpc <dotheanhpc@gmail.com>
2017-01-13 19:26:25 +0700

    Delete config.php

--- config.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$idacc = 'dotheanhpc';
-$access_token = 'EAAAAAYsX7TsBABeOBeUQjHS7a6J3tZCIxybql0Pn7xpvJJyyHl852WoEWFP1Wau33LOnZBggJIujHCCSEzc0RApCFGZCqPoZAScFc4qSoAnWiBsyzwZAMjNUxrY5xJt9ZC65uOnkFUP6ud18SIEPltUhDPEGde97TcizKAbi9ksudnvj3ZCcWMpY9u2zxntedDHd4liUnPGSgZDZD';
-?>
