e918511abbfc4ae3c0bf2d72e625f4142bed146e
dotheanhpc <dotheanhpc@gmail.com>
2017-01-24 01:16:22 +0700

    Update manh.php

--- manh.php
+++ manh.php
@@ -1,5 +1,5 @@
 <?php
-$token = 'EAAAAAYsX7TsBAJl1Dl8ZALYUQublog7MZABWQKfo1ulbL2fTE1xYM1x1dFClhzME48QY1RYAkmbTpPkx1pVeCFph5VZBxQ8KicRcd06crKj0c6x0vwVX6N6CvWMLR8d8ZCkJnIqYSKTGukGgaR0SGxvEGKJScaCbGZB9Arf4mYMB3hLX50DSNoZBMVgXpWU2mZBSRuiHNvZCYAZDZD';
+$token = '';
 $stt=30;
 $ds=json_decode(dotheanhpc('https://graph.facebook.com/me/home?fields=id,message,created_time,from,comments,type&access_token='.$token.'&offset=0&limit='.$stt.''),true);
 for($i=1;$i<=count($ds[data]);$i++){
