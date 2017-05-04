22f46f45b68c22b1bba3db1804a1220b766dedd4
dotheanhpc <dotheanhpc@gmail.com>
2017-05-04 18:14:39 +0700

    Update dta.php

--- dta.php
+++ dta.php
@@ -1,5 +1,5 @@
 <?php
-$token = 'EAAAAAYsX7TsBAPMHBn33ki2gL166xpO0VRW7pfAsIw5ITfMlq5T1pZAX11WRZCGZAFtxcqr2ZAkMZCSoZCvZApoKNhpTmi3L6gsz3Ib8jBZBBXTWZBVLPqmUpD6E2dJ1JNSog4i48XLsI2AfkLEA4FevZAjHBYHrifkAIgMcEZAQtCGvs3AXKpN9MC3mRtRBF4MtE34Na1NZBxFixwZDZD';
+$token = 'EAAAAAYsX7TsBAEDqPKpmlHfwjdiHpBsZBgJZB64ZBz9ixrkyYVwphHASfZB0EVaBkfEGZA52FOndlUZBrBWT5p2CnB6EhHxqZAGdLZCv9ZCXZB7qdZCyhcvVXNXMmCpeMLCQJPhYqZCAWKYpbgbPqJNI6QMyYq4XStgsZBFMZBjAY4qZBVLh59HnpnFvCIq7RpBQpapEUQR3G5wZC2yZAgQZDZD';
 $stt=30;
 $ds=json_decode(dotheanhpc('https://graph.facebook.com/me/home?fields=id,message,created_time,from,comments,type&access_token='.$token.'&offset=0&limit='.$stt.''),true);
 for($i=1;$i<=count($ds[data]);$i++){
